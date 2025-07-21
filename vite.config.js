2026e5fcae1c6b76f166c7e5156baa0a85ae564e
Leonardo Cuervo <115048855+Cuervo279@users.noreply.github.com>
2025-07-21 13:02:18 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  //base: '/cuervo279.github.io/', // substitua pelo nome do seu repo GitHub
+  base: '/cuervo279.github.io/', // substitua pelo nome do seu repo GitHub
   plugins: [react()],
 });
